e1e09bde45556c03685a5eda95fbbfa11ba74356
smaniacogit config --global user.name smaniaco <joaopedromarin@outlook.com.br>
2021-07-13 12:54:55 -0300

    Exercicios de JS feito

---
+++ semana3/semana3-aula1/js-vanilla-template/js-vanilla-template/src/index.js
@@ -0,0 +1,13 @@
+let numbers = [1,2,3,4,5,6]
+
+console.log(numbers[0],  numbers[2],  numbers[numbers.length - 1] )
+
+
+numbers[0] = 2
+
+numbers[2] = 6
+
+numbers[4] = 10
+
+console.log(numbers)
+
