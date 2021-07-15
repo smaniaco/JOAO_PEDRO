9c15f44fd71270a920de9cade3b291a97cada07c
smaniacogit config --global user.name smaniaco <joaopedromarin@outlook.com.br>
2021-07-15 14:02:26 -0300

    Exercicios e desafios FEITOS

---
+++ semana3/semana3-aula12/desafio2.js
@@ -0,0 +1,89 @@
+const name = prompt("Nome completo:")
+let gameType = prompt("Tipo de jogo (IN) internacional (DO) doméstico").toUpperCase()
+let gamePhase = prompt("(SF) semi-final (DT) decisão de terceiro lugar (FL) final").toUpperCase()
+const category = Number(prompt("Numero de 1 ao 4 para escolher a categoria (1 melhor e mais caro 4 pior e mais barato"))
+const quantity = Number(prompt("numero de ingressos"))
+
+let preco = 0
+
+
+switch (gamePhase){
+    case "SF":
+        gamePhase = "Semi-final"
+        switch(category){
+            case 1:
+                preco = 1320.00
+                break
+            case 2:
+                preco = 880.00
+                break
+            case 3:
+                preco = 550.00
+                break
+            case 4:
+                preco = 220.00
+                break
+        }
+    case "DT":
+        gamePhase = "Decisao do terceiro lugar"
+        switch(category){
+            case 1:
+                preco = 660.00
+                break
+            case 2:
+                preco = 440.00
+                break
+            case 3:
+                preco = 330.00
+                break
+            case 4:
+                preco = 170.00
+                break
+        }
+    case "FL":
+        gamePhase = "Final"
+        switch(category){
+            case 1:
+                preco = 1980.00
+                break
+            case 2:
+                preco = 1320.00
+                break
+            case 3:
+                preco = 880.00
+                break
+            case 4:
+                preco = 330.00
+                break
+        }
+    }
+
+if (gameType === "IN"){
+    preco /= 4.10
+    gameType= "Internacional"
+} 
+else {
+    gameType= "Doméstico"
+}
+
+
+
+const ingressos = preco * quantity
+
+
+console.log(`---Dados da Compra---`)
+console.log(`Nome do cliente: ${name}`)
+console.log(`Tipo de jogo: ${gameType}`)
+console.log(`Etapa do jogo: ${gamePhase}`)
+console.log(`Categoria: ${category}`)
+console.log(`Quantidade de ingressos: ${quantity}`)
+console.log(`---Valores---`)
+if (gameType === "Doméstico"){
+    console.log(`valor do ingresso R$${preco}`)
+    console.log(`valor total R$${ingressos}`)   
+} else {
+    console.log(`valor do ingresso U$${preco}`)
+    console.log(`valor total U$${ingressos}`)
+}
+
+
