20a8ef9ea202ea92a91d08f4c8c1b2633cb193b7
smaniacogit config --global user.name smaniaco <joaopedromarin@outlook.com.br>
2021-09-10 06:52:53 -0300

    teste

---
+++ semana6/semana6-aula23/boiler-plate/build/precache-manifest.dc5ece72ab1c570250e0d71721300cbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f298755b83c93068bea701885dc521e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26bf8c5ace345332c926",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "28e1f6e4c00be6f90c95",
+    "url": "/static/js/2.8fdc7c9c.chunk.js"
+  },
+  {
+    "revision": "26bf8c5ace345332c926",
+    "url": "/static/js/main.a2964b4a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
