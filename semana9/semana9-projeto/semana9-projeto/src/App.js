4a981cd5471489d503b443b96b2bbe4fece577b0
smaniacogit config --global user.name smaniaco <joaopedromarin@outlook.com.br>
2021-10-20 13:26:43 -0300

    projeto atualizado

--- semana9/semana9-projeto/semana9-projeto/src/App.js
+++ semana9/semana9-projeto/semana9-projeto/src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
-import styled from "styled-components"
-import MainComponent from "./Components/MainComponent/MainComponent"
-import 'antd/dist/antd.css'
-
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import MainComponent from "./Components/MainComponent/MainComponent";
+import "antd/dist/antd.css";
 
 const MainDiv = styled.div`
-  min-height:100vh;
-  display:flex;
+  min-height: 100vh;
+  display: grid;
+
+  grid-template-columns: 1fr 1fr 1fr 3fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  height: 100vh;
+`;
+
+const Header = styled.header`
+  grid-column: 1/-1;
+  grid-row: 1;
+  display: flex;
+  width: 100%;
+`;
+
+const Bar = styled.div`
+  & > h1 {
+    font-size: 3em;
+    text-align: center;
+    color: white;
+    font-family: "Courgette", cursive;
+    margin: 0;
+  }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50%;
+  width: 100%;
+  background: rgb(252, 69, 69);
+  background: linear-gradient(
+    180deg,
+    rgba(252, 69, 69, 1) 0%,
+    rgba(255, 144, 144, 1) 50%,
+    rgba(252, 69, 69, 1) 100%
+  );
+`;
+
+const Footer = styled.footer`
+  grid-row: 7/8;
+  grid-column: 1/-1;
+  display: flex;
+  align-items: flex-end;
+`;
+
+const FooterBar = styled.div`
+  & > p {
+    font-size: 1.3em;
+    text-align: center;
+    color: #222222;
+    font-family: "Roboto", sans-serif;
+  }
+  height: 50%;
+  width: 100%;
+  background: rgb(255, 255, 255);
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(232, 232, 232, 1) 50%,
+    rgba(255, 255, 255, 1) 100%
+  );
+  opacity: 0.9;
+  display: flex;
   justify-content: center;
   align-items: center;
- 
-`
+`;
 
 function App() {
   return (
     <MainDiv className="MainDiv">
-      
-      <MainComponent/>
-      </MainDiv>
+      <Header>
+        <Bar>
+          <h1>Love Finder</h1>
+        </Bar>
+      </Header>
+      <MainComponent />
+      <Footer>
+        <FooterBar>
+          <p>Todos os direitos reservados</p>
+        </FooterBar>
+      </Footer>
+    </MainDiv>
   );
 }
 
--- semana9/semana9-projeto/semana9-projeto/src/Components/MainComponent/MainComponent.js
+++ semana9/semana9-projeto/semana9-projeto/src/Components/MainComponent/MainComponent.js
@@ -1,453 +1,454 @@
-import React, {useEffect, useState} from 'react'
-import styled from "styled-components"
-import axios from "axios"
-import {Card} from "antd"
-import Lottie from 'react-lottie';
-import animationData from "./lotties/heart"
-import rocket from "./lotties/rocket-launch.json"
-import check from "./svg/check-sign.svg"
-import refuse from "./svg/deny-sign.svg"
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { Card } from "antd";
+import Lottie from "react-lottie";
+import animationData from "./lotties/heart";
+import rocket from "./lotties/rocket-launch.json";
+import check from "./svg/check-sign.svg";
+import refuse from "./svg/deny-sign.svg";
 
 const MainDiv = styled.div`
-  font-size:16px;
-  height:600px;
-  @media (max-width:2500px){
-    height:1300px;
-    font-size:2em;
+  grid-column: 4/5;
+  grid-row: 2/7;
+  border-radius: 10px;
+  row-gap: 5%;
+  background-color: white;
+  display: grid;
+  padding: 1vw;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+  @media (max-width: 1000px) {
+    grid-column: 3/6;
+    grid-row: 2/7;
   }
-  @media (max-width:2200px){
-    height:1200px;
-    font-size:1.2em;
+  @media (max-width: 700px) {
+    grid-column: 2/7;
+    grid-row: 2/7;
   }
-  @media (max-width:1900px){
-    font-size:1em;
-    height:1000px;
+  @media (max-width: 1024px) and (min-width: 1020px) and (max-height: 1366px) and (min-height: 1350px) {
+    grid-column: 2/7;
+    grid-row: 2/7;
   }
-  @media (max-width:1500px){
-    height:850px;
-  }
-  @media (max-width:1300px){
-    height:800px;
-  }
-  @media (max-width:1000px){
-    font-size:1em;
-    height:660px;
-    width:50%;
-  }
-  @media (max-width:800px){
-    height:650px;
-    width:60%;
-  }
-  @media (max-width:600px){
-    height:650px;
-    width:70%;
-  }
-  @media (max-width:500px){
-    height:600px;
-    width:80%;
-  }
-  @media (max-width:400px){
-    height:550px;
-    width:90%;
-  }
-  @media (max-width:1980px) and (max-height:1300px) and (min-height:1100px){
-    height:950px;
-    width:40%;
- 
-  }
-  @media (max-width:400px) and (max-height:850px){
-    height:650px;
-  }
+`;
 
-  width:40%;
-  background-color:white;
-  border-radius:15px;
-  border:1px black solid;
-  padding:1vw;
-`
+const MatchDivContainer = styled.div`
+  grid-column: 1/-1;
+  grid-row: 1/-1;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+`;
+
+const MatchDivSecondContainer = styled.div`
+  grid-column: 1/-1;
+  grid-row: 1/-1;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+`;
 const Title = styled.h2`
-  color:white;
-`
+  color: white;
+  @media (min-width: 1700px) {
+    font-size: 2em;
+  }
+`;
 const Description = styled.h3`
-  margin:0 !important;
-  color:white;
-`
+  margin: 0 !important;
+  color: white;
+  @media (min-width: 1700px) {
+    font-size: 1.5em;
+  }
+`;
 
 const TextDiv = styled.div`
-  z-index:4;
-  padding:5px;
-  display:flex;
+  z-index: 4;
+  padding: 5px;
+  display: flex;
   flex-direction: column;
   justify-content: flex-end;
-  background: linear-gradient(0deg, rgba(0,0,0,1) 40%, rgba(0,0,0,0) 100%);
-  width:100%;
-  grid-row:4/5;
-  grid-column:1/-1;
-  
-`
+  background: linear-gradient(
+    0deg,
+    rgba(0, 0, 0, 1) 40%,
+    rgba(0, 0, 0, 0) 100%
+  );
+  width: 100%;
+  grid-row: 4/5;
+  grid-column: 1/-1;
+`;
 const ButtonDiv = styled.div`
-  margin-top:10px;
-  z-index:110;
-  width:100%;
-  min-height:10%;
- 
+  z-index: 110;
+  width: 100%;
+  min-height: 10%;
+  grid-row: 5/7;
+`;
 
-`
-const ButtonDivBoxCheck = styled.div`
-  width:100%;
-  grid-column: 1;
-`
-const ButtonDivBoxRefuse = styled.div`
-  width:100%;
-  grid-column: 3;
-
-`
-
-
-const MatchesContainer = styled.div`
-  
-    height:100%;
-  
-
-`
-const MatchesContainerFather = styled.div`
-@media (max-height:800px){
-  height:80%;
-}
-  
-    height:100%;
-  
-
-`
 const MatchesContainerLoading = styled.div`
-  padding-bottom:20%;
-  display:flex;
+  padding-bottom: 20%;
+  display: flex;
   justify-content: center;
   align-items: center;
-  height:100%;
-  
-
-`
+  height: 100%;
+  grid-column: 2/4;
+  grid-row: 3/4;
+`;
 const ImageMatch = styled.img`
-  width:8vw;
-  height:8vw;
-  margin-right:5vw;
-  border-radius:280px;
-`
+  width: 8vw;
+  height: 8vw;
+
+  border-radius: 280px;
+  @media (max-width: 1000px) {
+    width: 12vw;
+    height: 12vw;
+  }
+  @media (max-width: 700px) {
+    width: 18vw;
+    height: 18vw;
+  }
+  @media (max-width: 600px) {
+    width: 22vw;
+    height: 22vw;
+  }
+`;
 const MatchDiv = styled.div`
-  display:flex;
+  display: flex;
+  justify-content: space-between;
   align-items: center;
-  margin:2vh 0;
-`
+  margin: 2vh 0;
+`;
 
 const MatchListDiv = styled.div`
-  height:100%;
-  & > button{
-    font-size:2vw;
+  grid-row: 2/-1;
+  grid-column: 1/-1;
+  height: 100%;
+  & > button {
+    font-size: 2vw;
   }
   overflow-y: auto;
-`
+`;
 
 const MatchListContainer = styled.div`
-
-  height:80%;
-
-`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+  grid-row: 1/-1;
+  grid-column: 1/-1;
+`;
 
 const TopPanel = styled.div`
-  display:flex;
-  justify-content: space-between;
-  margin-bottom:5px;
-  height:10%;
-  margin-bottom:20px;
-  
-`
+  height: 100%;
+  width: 100%;
+  z-index: 10;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr;
+  grid-row: 1/2;
+  grid-column: 1/-1;
+`;
 
 const ImageCandidate = styled.img`
+  max-height: 100%;
+  width: auto;
+  max-width: 100%;
+  height: 100%;
+  @media (max-width: 1024px) and (min-width: 1020px) and (max-height: 1366px) and (min-height: 1350px) {
+    height: 100%;
+    width: auto;
+  }
 
-  max-height:100%;
-  width:auto;
-  max-width:100%;
-  height:auto;
-
-
-  z-index:100;
-  
-  
-`
+  z-index: 100;
+`;
 const ImageCandidateDiv = styled.div`
-  display:flex;
+  box-sizing: border-box;
+  display: flex;
   justify-content: center;
   align-items: center;
-  width:100%;
-  height:100%;
-  grid-row:1/-1;
-  grid-column:1/-1;
-  z-index:3;
-`
+  width: 100%;
+  height: 100%;
+  grid-row: 1/-1;
+  grid-column: 1/-1;
+  z-index: 3;
+`;
 
 const Check = styled.img`
-  margin:0 auto;
-  margin-top:5px;
-  position:absolute;
-  left:35%;
+  margin: 0 auto;
+  margin-top: 5px;
+  position: absolute;
+  left: 35%;
   filter: brightness(0);
-  width:9%;
-  height:9%;
+  width: 9%;
+  height: 9%;
   &:hover {
-    transition:1000ms;
+    transition: 1000ms;
     transform: scale(1.2);
     filter: brightness(100%);
   }
-  &:active{
+  &:active {
     filter: brightness(30%);
   }
-  @media (max-width:600px){
-    left:20%;
-    width:15%;
-  height:15%;
-  
+  @media (max-width: 600px) {
+    width: 13%;
+    height: 13%;
+    left: 20%;
   }
-  @media (max-height:500px){
-    left:30%;
-    width:8vw;
-  height:8vw;
-  }
-  
-`
+`;
 const Refuse = styled.img`
-  
-  position:absolute;
-  margin-top:5px;
-  left:56%;
- 
-  width:9%;
-  height:9%;
+  position: absolute;
+  margin-top: 5px;
+  left: 56%;
+
+  width: 9%;
+  height: 9%;
   filter: brightness(0);
   &:hover {
-    
-    transition:1000ms;
+    transition: 1000ms;
     filter: brightness(100%);
-    transform: scale(1.2)
+    transform: scale(1.2);
   }
-  &:active{
+  &:active {
     filter: brightness(30%);
   }
-  @media (max-width:600px){
-    width:15%;
-  height:15%;
-  left:65%;
+  @media (max-width: 600px) {
+    width: 13%;
+    height: 13%;
+    left: 67.3%;
   }
-  @media (max-height:500px){
-    left:62%;
-    width:8vw;
-  height:8vw;
-  }
-`
+`;
 const BackPhoto = styled.img`
-  grid-row:1/-1;
-  grid-column:1/-1;
-  filter:blur(3px);
-  height:100%;
-  width:100%;
- 
-`
+  box-sizing: border-box;
 
-
-
+  grid-row: 1/-1;
+  grid-column: 1/-1;
+  filter: sepia(60%) blur(2px);
+  height: 100%;
+  width: 100%;
+`;
 
 const MatchesMainDiv = styled.div`
-  display:grid;
+  box-sizing: border-box;
+  display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr 1fr;
-  height:70%;
-  
-  width:100%;
- 
-`
+  height: 70%;
+  grid-row: 2/6;
+  grid-column: 1/-1;
+  width: 100%;
+`;
 
 const NoMatches = styled.div`
-  
-  text-align:center;
-  flex-direction:column;
-  display:flex;
-  align-items:center;
-  justify-content: center;
-  height:100%;
-  width:100%;
-`
-const LoadingMatchesContainer = styled.div`
-  width:100%;
-  height:100%;
-  display:flex;
+  grid-row: 2/-1;
+  grid-column: 1/-1;
+  text-align: center;
+  flex-direction: column;
+  display: flex;
   align-items: center;
   justify-content: center;
-`
+  height: 100%;
+  width: 100%;
+`;
+const LoadingMatchesContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  grid-row: 1/-1;
+  grid-column: 1/-1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const MatchText = styled.div`
+  width: 60%;
+`;
 
-const CorpNote = styled.p`
-  text-align:center;
-  opacity:0.5;
-  color:#222222;
-`
+const MatchListIcon = styled.div`
+  width: 100%;
+  height: 100%;
+  grid-row: 1/1;
+  grid-column: 1/1;
+`;
+const ClearListIcon = styled.div`
+  width: 100%;
+  height: 100%;
+  margin-left: 10%;
+  grid-row: 1/1;
+  grid-column: 4/5;
+`;
 
 export function MainComponent(props) {
-  let [profiles, setProfiles] = useState({})
-  let [loading, setLoading] = useState(false)
-  let [matchList, setMatchListOn] = useState(false)
-  let [matches, setMatches] = useState([])
-  let [defaultOptions, setDefaultOptions ]= useState({
+  let [profiles, setProfiles] = useState({});
+  let [loading, setLoading] = useState(false);
+  let [matchList, setMatchListOn] = useState(false);
+  let [matches, setMatches] = useState([]);
+  let [profileChange, setChange] = useState(false);
+  let [defaultOptions, setDefaultOptions] = useState({
     loop: true,
     autoplay: true,
     animationData: animationData,
     rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }})
-  let [defaultOptionsRocket, setDefaultOptionsRocket ]= useState({
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  });
+  let [defaultOptionsRocket, setDefaultOptionsRocket] = useState({
     loop: true,
     autoplay: true,
     animationData: rocket,
     rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }})
-  let [loadingMatches, setLoadingMatches] =  useState(false)
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  });
+  let [loadingMatches, setLoadingMatches] = useState(false);
 
-  const getProfile =  () =>{
-    
-    axios.get("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/darvas/person").then(msg=>{
-      setProfiles(msg.data)
-      setLoading(true)
-    }).catch((error)=>{
-      console.log(error)
-    })
-    console.log(profiles)
-    
-  }
-  const getMatches = ()=>{
-    axios.get("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/darvas/matches").then(msg=>{
-      setMatches([...msg.data.matches])
-      setLoadingMatches(true)
-    }).catch((error)=>{
-      console.log(error)
-    })
-    console.log(profiles)
-    
-  }
-  const choosePerson =(state)=>{
+  const getProfile = () => {
+    axios
+      .get(
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/darvas/person"
+      )
+      .then((msg) => {
+        setProfiles(msg.data);
+        setLoading(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log(profiles);
+  };
+  const getMatches = () => {
+    axios
+      .get(
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/darvas/matches"
+      )
+      .then((msg) => {
+        setMatches([...msg.data.matches]);
+        setLoadingMatches(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log(profiles);
+  };
+  const choosePerson = (state) => {
     const body = {
-      "id": profiles.profile.id,
-      "choice": state
-    }
-    axios.post("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/darvas/choose-person", body).then(msg=>{
-     console.log(msg)
-    }).catch((error)=>{
-      console.log(error)
-    })
+      id: profiles.profile.id,
+      choice: state,
+    };
+    axios
+      .post(
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/darvas/choose-person",
+        body
+      )
+      .then((msg) => {
+        console.log(msg);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-    getProfile()
-    
-  }
-  const clear =()=>{
-    
-    axios.put("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/darvas/clear").then(msg=>{
-     console.log(msg)
-    }).catch((error)=>{
-      console.log(error)
-    })
-    getProfile()
-    matchListOn()
-    
-  }
-  const matchListOn = ()=>{
-    setMatchListOn(!matchList)
-    getMatches()
-  }
-  const matchElements = matches.map((match)=>{
-    return <MatchDiv>
-    <ImageMatch src={match.photo}/>
-    <h1>{match.name}</h1>
- 
-    </MatchDiv>
-  })
-  
-  useEffect (()=>{
-    
-    getProfile()
-  },[])
+    getProfile();
+  };
+  const clear = () => {
+    axios
+      .put(
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/darvas/clear"
+      )
+      .then((msg) => {
+        console.log(msg);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    getProfile();
+    matchListOn();
+  };
+  const matchListOn = () => {
+    setMatchListOn(!matchList);
+    getMatches();
+    setChange(!profileChange);
+  };
+  const matchElements = matches.map((match) => {
+    return (
+      <MatchDiv>
+        <ImageMatch src={match.photo} />
+        <MatchText>
+          <h1>{match.name}</h1>
+        </MatchText>
+      </MatchDiv>
+    );
+  });
 
-
+  useEffect(() => {
+    getProfile();
+  }, [profileChange]);
 
   return (
     <MainDiv>
-      
-      <TopPanel >
-      <div onClick={matchListOn} className="play-on-hover">
-      </div>
-      
-      {matchList ? <div className="on-hover-clear" onClick={clear}></div> : ""}
+      <TopPanel>
+        <MatchListIcon onClick={matchListOn} className="play-on-hover" />
+        {matchList ? (
+          <ClearListIcon
+            className="on-hover-clear"
+            onClick={clear}
+          ></ClearListIcon>
+        ) : (
+          ""
+        )}
       </TopPanel>
-      {matchList ? 
-      <MatchListContainer>
-       {loadingMatches === true ?
-      <MatchListDiv className="scroll">
-        
-        {matchElements}
-      </MatchListDiv>:<LoadingMatchesContainer><Lottie 
-      
-	    options={defaultOptions}
-        height={"5vw"}
-        width={"5vw"}
-      />
-      </LoadingMatchesContainer>
-      }
-      </MatchListContainer>:
-      <MatchesContainerFather>
-      {loading ?
-      <MatchesContainer>
-      
-      {profiles.profile === null ? <NoMatches><h1>Parece que você voou muito alto!
-        Você ja viu todos candidatos</h1>
-        <Lottie 
-      
-	    options={defaultOptionsRocket}
-        height={"15vw"}
-        width={"15vw"}
-      />
-      </NoMatches>:
-      <MatchesContainer>
-      <MatchesMainDiv>
-        <BackPhoto src={profiles.profile.photo}/>
-        <ImageCandidateDiv>
-        <ImageCandidate src={profiles.profile.photo}/>
-        </ImageCandidateDiv>
-        <TextDiv>
-      <Title>{profiles.profile.name}, {profiles.profile.age}</Title>
-      <Description>{profiles.profile.bio}</Description>
-      </TextDiv>
-      </MatchesMainDiv>
-      <ButtonDiv>
-      <Check onClick={() => choosePerson(true)} src={check}/>
-      <Refuse onClick={()=> choosePerson(false)} src={refuse}/>
-      
-      </ButtonDiv>
-    </MatchesContainer>
-} 
-    
-    </MatchesContainer> 
-      :<MatchesContainerLoading><Lottie 
-      
-	    options={defaultOptions}
-        height={"5vw"}
-        width={"5vw"}
-      /></MatchesContainerLoading>} 
-   
 
-      </MatchesContainerFather>
-
-
-      }
-      </MainDiv>
+      {matchList ? (
+        <MatchListContainer>
+          {loadingMatches === true ? (
+            <MatchListDiv className="scroll">{matchElements}</MatchListDiv>
+          ) : (
+            <LoadingMatchesContainer className="teste">
+              <Lottie options={defaultOptions} height={"5vw"} width={"5vw"} />
+            </LoadingMatchesContainer>
+          )}
+        </MatchListContainer>
+      ) : (
+        <MatchDivSecondContainer>
+          {profiles.profile === null ? (
+            <NoMatches>
+              <h1>
+                Parece que você voou muito alto! Você ja viu todos candidatos
+              </h1>
+              <Lottie
+                options={defaultOptionsRocket}
+                height={"15vw"}
+                width={"15vw"}
+              />
+            </NoMatches>
+          ) : (
+            <MatchDivContainer>
+              {loading ? (
+                <MatchesMainDiv>
+                  <BackPhoto src={profiles.profile.photo} />
+                  <ImageCandidateDiv>
+                    <ImageCandidate src={profiles.profile.photo} />
+                  </ImageCandidateDiv>
+                  <TextDiv>
+                    <Title>
+                      {profiles.profile.name}, {profiles.profile.age}
+                    </Title>
+                    <Description>{profiles.profile.bio}</Description>
+                  </TextDiv>
+                </MatchesMainDiv>
+              ) : (
+                <MatchesContainerLoading>
+                  <Lottie
+                    options={defaultOptions}
+                    height={"5vw"}
+                    width={"5vw"}
+                  />
+                </MatchesContainerLoading>
+              )}
+              <ButtonDiv>
+                <Check onClick={() => choosePerson(true)} src={check} />
+                <Refuse onClick={() => choosePerson(false)} src={refuse} />
+              </ButtonDiv>
+            </MatchDivContainer>
+          )}
+        </MatchDivSecondContainer>
+      )}
+    </MainDiv>
   );
 }
-
 
 export default MainComponent;
